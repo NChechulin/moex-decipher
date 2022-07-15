f61e483d7ae9301c17435dfc866711b4b08df617
Nikolay Chechulin <nchechulin@protonmail.com>
2022-07-15 23:34:21 +0300

    add cipher

---
+++ moex_decipher/cipher.py
@@ -0,0 +1,33 @@
+from copy import copy
+from random import shuffle
+from string import ascii_lowercase
+from typing import Dict
+
+
+def generate_letter_mapping() -> Dict[str, str]:
+    """Generates a mapping from an english letter to another one (possibly, same)"""
+    letters = list(ascii_lowercase)
+    shuffled = copy(letters)
+    shuffle(letters)
+
+    return dict(zip(letters, shuffled))
+
+
+def encode(text: str, mapping: Dict[str, str]) -> str:
+    """Encodes given text with a given mapping skipping all non-ascii chars"""
+    return "".join(
+        map(
+            lambda c: mapping.get(c, c),
+            text.lower(),
+        )
+    )
+
+
+def main() -> None:
+    letter_map = generate_letter_mapping()
+    encoded = encode("abobus prikolus", letter_map)
+    print(encoded)
+
+
+if __name__ == "__main__":
+    main()
